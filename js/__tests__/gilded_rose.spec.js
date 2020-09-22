2b92301a7e5e388396e42f86342f0d42e0a8f459
amdshehab <shehab.ahmed.uk@gmail.com>
2020-09-22 11:18:46 +0100

    chore: refactors tests

--- js/__tests__/gilded_rose.spec.js
+++ js/__tests__/gilded_rose.spec.js
@@ -2,74 +2,115 @@ import { Item, updateQuality } from '../src/gilded_rose'
 
 describe("Gilded Rose", function () {
 
-  it("should foo", function () {
-    const items = [new Item("foo", 0, 0)];
-    updateQuality(items);
-    expect(items[0].name).toEqual("foo");
-  });
+  describe("Standard items", () => {
+    it("should lower the sell_in and quality by 1", () => {
+      const items = [new Item("foo", 12, 5)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "foo",
+        sell_in: 11,
+        quality: 4
+      }])
+    })
 
-  it("should lower the sell_in and quality by 1", () => {
-    const items = [new Item("foo", 12, 5)];
+    it("should degrade quality twice as fast when sell_in is less than 0", () => {
+      const items = [new Item("foo", -1, 5)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "foo",
+        sell_in: -2,
+        quality: 3
+      }])
+    })
 
-    expect(updateQuality(items)).toMatchObject([{
-      name: "foo",
-      sell_in: 11,
-      quality: 4
-    }])
+    it("shouldn't degrade quality beyond 0", () => {
+      const items = [new Item("foo", -1, 1)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "foo",
+        sell_in: -2,
+        quality: 0
+      }])
+    })
   })
 
-  it("should degrade quality twice as fast when sell_in is less than 0", () => {
-    const items = [new Item("foo", -1, 5)];
-    expect(updateQuality(items)).toMatchObject([{
-      name: "foo",
-      sell_in: -2,
-      quality: 3
-    }])
+
+  describe("Aged Brie", () => {
+    it('should increment quality for Aged Brie', () => {
+      const items = [new Item("Aged Brie", 4, 3)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Aged Brie",
+        sell_in: 3,
+        quality: 4
+      }])
+    })
+
+    it('should increment quality twice as fact for Aged Brie - sellIn expired', () => {
+      const items = [new Item("Aged Brie", -1, 4)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Aged Brie",
+        sell_in: -2,
+        quality: 6
+      }])
+    })
+
+    it("Quality shouldn't increment beyond 50", () => {
+      const items = [new Item("Aged Brie", -1, 49)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Aged Brie",
+        sell_in: -2,
+        quality: 50
+      }])
+    })
   })
 
-  it("shouldn't degrade quality beyond 0", () => {
-    const items = [new Item("foo", -1, 1)];
-    expect(updateQuality(items)).toMatchObject([{
-      name: "foo",
-      sell_in: -2,
-      quality: 0
-    }])
+
+  describe("Sulfuras", () => {
+    it("shouldn't update anything for Sulfuras", () => {
+      const items = [new Item("Sulfuras, Hand of Ragnaros", 5, 5)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Sulfuras, Hand of Ragnaros",
+        sell_in: 5,
+        quality: 5
+      }])
+    })
   })
 
-  it('should increment quality for Aged Brie', () => {
-    const items = [new Item("Aged Brie", 4, 3)];
-    expect(updateQuality(items)).toMatchObject([{
-      name: "Aged Brie",
-      sell_in: 3,
-      quality: 4
-    }])
+
+  describe("Backstage Passes", () => {
+    it("increases quality by 1 for backstage passes - SellIn > 10", () => {
+      const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 14,
+        quality: 11
+      }])
+    })
+
+    it("increases quality by 2 for backstage passes - SellIn <= 10", () => {
+      const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 8,
+        quality: 12
+      }])
+    })
+
+    it("increases quality by 3 for backstage passes - SellIn <= 5", () => {
+      const items = [new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: 4,
+        quality: 13
+      }])
+    })
+
+    it("sets quality to 0 after concert - SellIn < 0", () => {
+      const items = [new Item("Backstage passes to a TAFKAL80ETC concert", -1, 10)];
+      expect(updateQuality(items)).toMatchObject([{
+        name: "Backstage passes to a TAFKAL80ETC concert",
+        sell_in: -2,
+        quality: 0
+      }])
+    })
   })
 
-  it('should increment quality twice as fact for Aged Brie - sellIn expired', () => {
-    const items = [new Item("Aged Brie", -1, 4)];
-    expect(updateQuality(items)).toMatchObject([{
-      name: "Aged Brie",
-      sell_in: -2,
-      quality: 6
-    }])
-  })
-
-  it("Quality shouldn't increment beyond 50", () => {
-    const items = [new Item("Aged Brie", -1, 49)];
-    expect(updateQuality(items)).toMatchObject([{
-      name: "Aged Brie",
-      sell_in: -2,
-      quality: 50
-    }])
-  })
-
-  it("shouldn't update anything for Sulfuras", () => {
-    const items = [new Item("Sulfuras", 5, 5)];
-    expect(updateQuality(items)).toMatchObject([{
-      name: "Sulfuras",
-      sell_in: 5,
-      quality: 5
-    }])
-  })
 
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -28,7 +28,19 @@ const decrementQuality = (quality, amount = 1) => {
 
 const handleQualityUpdate = (name, quality, sell_in) => {
 
-  if (name === 'Sulfuras') return quality
+  if (name === 'Sulfuras, Hand of Ragnaros') return quality
+
+  if (name === 'Backstage passes to a TAFKAL80ETC concert') {
+    if (sell_in < 0) return 0
+    if (sell_in > 10) {
+      return incrementQuality(quality)
+    } else if (sell_in <= 5) {
+      return incrementQuality(quality, 3)
+    } else {
+      return incrementQuality(quality, 2)
+    }
+  }
+
   if (name === 'Aged Brie') {
     return sell_in >= 0 ? incrementQuality(quality) : incrementQuality(quality, 2)
   }
@@ -41,72 +53,17 @@ const handleQualityUpdate = (name, quality, sell_in) => {
 }
 
 const handleSellInUpdate = (name, sell_in) => {
-  if (name === "Sulfuras") return sell_in
+  if (name === "Sulfuras, Hand of Ragnaros") return sell_in
   return sell_in - 1
 }
 
 function updateQuality(items) {
 
-  const updatedItems = items.map(({ name, quality, sell_in }) => ({
+  return items.map(({ name, quality, sell_in }) => ({
     name,
     sell_in: handleSellInUpdate(name, sell_in),
     quality: handleQualityUpdate(name, quality, sell_in)
   }))
-
-  return updatedItems
-
-
-
-
-
-  // for (let i = 0; i < items.length; i++) {
-
-  //   if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-  //     if (items[i].quality > 0) {
-  //       if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //         items[i].quality = items[i].quality - 1
-  //       }
-  //     }
-  //   } else {
-  //     if (items[i].quality < 50) {
-  //       items[i].quality = items[i].quality + 1
-  //       if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-  //         if (items[i].sell_in < 11) {
-  //           if (items[i].quality < 50) {
-  //             items[i].quality = items[i].quality + 1
-  //           }
-  //         }
-  //         if (items[i].sell_in < 6) {
-  //           if (items[i].quality < 50) {
-  //             items[i].quality = items[i].quality + 1
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //     items[i].sell_in = items[i].sell_in - 1;
-  //   }
-  //   if (items[i].sell_in < 0) {
-  //     if (items[i].name != 'Aged Brie') {
-  //       if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-  //         if (items[i].quality > 0) {
-  //           if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-  //             items[i].quality = items[i].quality - 1
-  //           }
-  //         }
-  //       } else {
-  //         items[i].quality = items[i].quality - items[i].quality
-  //       }
-  //     } else {
-  //       if (items[i].quality < 50) {
-  //         items[i].quality = items[i].quality + 1
-  //       }
-  //     }
-  //   }
-  // }
-
-  return items
 }
 
 export {
